281a7c853795b4678e549d607be05cf021b4bc54
Kailash Tuta <62685303+kailashTuta@users.noreply.github.com>
2021-10-18 09:20:07 +0530

    Connected to mongodb using mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
 
-// ROUTES
-app.get
 
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => {
+        console.log('Connected to DB!')
+    }
+);
 
 app.listen(5000);
 
