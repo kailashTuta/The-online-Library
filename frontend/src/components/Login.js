2fa1c6e51500a61401e738b3fcb7d90cc9d4764a
Kailash Tuta <62685303+kailashTuta@users.noreply.github.com>
2021-11-03 11:48:28 +0530

    Added Login page UI

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,24 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <div className="wrapper mt-5">
-                <div className="title">
-                    Login Form
-                </div>
-                <div className="form">
-                    <div className="inputfield">
-                        <label>Email</label>
-                        <input type="email" className="input" />
-                    </div>
-                    <div className="inputfield">
-                        <label>Password</label>
-                        <input type="password" className="input" />
-                    </div>
-                    <div className="inputfield">
-                        <input type="submit" value="Login" className="btn btn-primary" />
-                    </div>
+            <div className="container-signin">
+                <div className="signin-more"></div>
+                <div className="wrap-signin">
+                    <form action="" className="signin-form">
+                        <span className="signin-form-title">Log in</span>
+
+                        <div className="wrap-input">
+                            <span className="label-input">Email</span>
+                            <input className="input" type="email" name="email" placeholder="Email Address" />
+                            <span className="focus-input"></span>
+                        </div>
+
+                        <div className="wrap-input">
+                            <span className="label-input">Password</span>
+                            <input className="input" type="password" name="password" placeholder="**********" />
+                            <span className="focus-input"></span>
+                        </div>
+
+                        <div className="container-signin-form-btn">
+                            <div className="wrap-signin-form-btn">
+                                <button className="signin-form-btn">Login</button>
+                            </div>
+                            <Link className="signup-button hov1" to="/signup">Sign up <i className="fas fa-user-plus"></i></Link>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
